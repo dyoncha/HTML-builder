0e908af25dcacd1923684d3f036db8a43ac46177
dyoncha <dyoncha77@gmail.com>
2025-01-21 18:33:36 +0300

    fix: use ReadStream to read a file (01-read-file)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,13 @@
-const fs = require('node:fs/promises');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 
-async function readFile() {
-  const data = await fs.readFile(path.join(__dirname, 'text.txt'));
-  console.log(data.toString());
+function readFile() {
+  const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+  let data = '';
+
+  stream.on('data', (chunk) => (data += chunk));
+  stream.on('end', () => console.log(data));
+  stream.on('error', (err) => console.error('Error:', err));
 }
 
 readFile();
